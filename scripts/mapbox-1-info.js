7bc5a1effe954c43b867c4a2a5909d8e2664b2f1
Gerardo Ezequiel <geraezemc@gmail.com>
2020-11-12 11:21:49 +0000

    add meetinbetween

--- scripts/mapbox-1-info.js
+++ scripts/mapbox-1-info.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken =
   'pk.eyJ1IjoiZ2VyYWV6ZW1jIiwiYSI6ImNqM3N4YTY5ODAwNjYzMXFtd21peHp1b2sifQ.A-Y5AaoJWzn7tXFa1vvmlQ';
 
-/* var getLocation = function () {
+
+
+var getLocation = function () {
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
       (position) =>
@@ -10,14 +12,20 @@ mapboxgl.accessToken =
     );
   });
 };
-
 // to use it:
-
 getLocation()
-  .then((arrLocations) => console.log(arrLocations))
+  .then(function (data) {
+    userLocation = data;
+    map.flyTo({ center: userLocation, zoom: 15 });
+  })
   .catch((err) => console.log('there was an error: ', err));
-
- */
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  attributionControl: false,
+  // center: [-0.0844093, 51.4894947],
+  // zoom: 15,
+});
 
 var map = new mapboxgl.Map({
   container: 'map',
@@ -25,7 +33,7 @@ var map = new mapboxgl.Map({
   center: [-0.0844093, 51.4894947],
   zoom: 8,
   attributionControl: false,
-});
+}); 
 
 function HelloWorldControl() {}
 
