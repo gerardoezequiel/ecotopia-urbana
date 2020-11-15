896ba04396ee01aaa37c886e8a2edf6c1bc37aff
Gerardo Ezequiel <geraezemc@gmail.com>
2020-11-15 00:13:44 +0000

    cleaning files and routes

--- scripts/otm-popup.js
+++ scripts/otm-popup.js
@@ -1,5 +1,18 @@
 //Query for the pop-up information
-/* export const addOtmPopUp = ({ map, apiGet, onShowPOI }) => {
+export const addOtmPopUp = ({ apiGet, onShowPOI, map }) => {
+  //Open trip map
+  const apiKey = '5ae2e3f221c38a28845f05b6ed0662748f2fdf24cede18cf28fcee8a';
+
+  const apiGet = async (method, query) => {
+    const url =
+      query !== undefined
+        ? `https://api.opentripmap.com/0.1/en/places/${method}?apikey=${apiKey}&${query}`
+        : `https://api.opentripmap.com/0.1/en/places/${method}?apikey=${apiKey}`;
+
+    const response = await fetch(url);
+    return await response.json();
+  };
+
   function onShowPOI(data, lngLat) {
     let poi = document.createElement('div');
     poi.innerHTML = '<h2>' + data.name + '<h2>';
@@ -19,12 +32,16 @@
       "'>Show more at OpenTripMap</a></p>";
 
     new mapboxgl.Popup().setLngLat(lngLat).setDOMContent(poi).addTo(map);
+    const popup = document.getElementsByClassName('mapboxgl-popup');
+    if (popup.length) {
+      popup[0].remove();
+    }
   }
-
-  map.on('dblclick', 'opentripmap-pois', function (e) {
-    let coordinates = e.features[0].geometry.coordinates.slice();
-    let id = e.features[0].properties.id;
-    let name = e.features[0].properties.name;
+  map.on('mouseenter', 'opentripmap-pois', function (e) {
+    map.getCanvas().style.cursor = 'pointer';
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var id = e.features[0].properties.id;
+    var name = e.features[0].properties.name;
 
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
@@ -60,6 +77,4 @@
     map.getCanvas().style.cursor = '';
     popup.remove();
   });
-
 };
-  */
